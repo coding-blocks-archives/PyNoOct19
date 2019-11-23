e7660dda58ce0dec9b1ebb4cf37904bfb99cfb66
Jatin Katyal <jatin.katyal13@gmail.com>
2019-11-23 19:01:20 +0530

    flask templates complete

--- flask_application/app.py
+++ flask_application/app.py
@@ -1,16 +1,46 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-forms = []
+
+USERS = [
+  {
+    "name": "Jatin",
+    "language": "python",
+    "designation": "Developer"
+  },
+  {
+    "name": "Bipin",
+    "language": "python",
+    "designation": "Developer"
+  },
+  {
+    "name": "Rahul",
+    "language": "python",
+    "designation": "Developer"
+  },
+]
+
+@app.route('/users')
+def users():
+  return render_template('users.html', users = USERS, enumerate=enumerate)
+
+@app.route('/users/<int:id>')
+def user(id):
+  return render_template('user.html', user = USERS[id])
+
 
 @app.route('/')
 def index():
-  print(forms)
   return render_template('index.html')
 
 @app.route('/feedbackSubmit', methods = ['POST'])
 def feedbackSubmit():
-  forms.append(dict(request.form))
+  with open('data.csv', 'a') as file:
+    form = dict(request.form)
+    values = []
+    for v in form.values():
+      values.append(v[0])
+    file.write(",".join(values) + '\n')
   return "Success !"
 
 @app.route('/feedback')
